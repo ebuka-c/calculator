f20bd21c0a91b020137651f8c3dd6c291967a0bf
ebuka-c <chikodi873@gmail.com>
2023-01-19 12:42:46 +0100

    started calculator app

---
+++ lib/calculator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Calcualtor extends StatefulWidget {
+  const Calcualtor({Key? key}) : super(key: key);
+
+  @override
+  State<Calcualtor> createState() => _CalcualtorState();
+}
+
+class _CalcualtorState extends State<Calcualtor> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
